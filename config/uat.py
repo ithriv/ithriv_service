0cc25ff2c0ab9eaed884d033ae47d5c94ab2511f
Ravi Chamakuri <rkc7h@virginia.edu>
2019-06-10 15:46:29 -0400

    Pushing change for UAT https test

--- config/uat.py
+++ config/uat.py
@@ -17,7 +17,7 @@ DEVELOPMENT = True
 SSO_ATTRIBUTE_MAP['uid'] = (False, 'uid')
 SSO_DEVELOPMENT_EPPN = 'ithriv_admin@virginia.edu'
 
-API_URL = 'http://uat.ithriv.org'
-SITE_URL = 'http://uat.ithriv.org'
+API_URL = 'https://uat.ithriv.org'
+SITE_URL = 'https://uat.ithriv.org'
 FRONTEND_AUTH_CALLBACK, FRONTEND_EMAIL_RESET, FRONTEND_EMAIL_CONFIRM = auth_callback_url_tuple(
     SITE_URL, '/#/session', '/#/reset_password/', '/#/login/')
